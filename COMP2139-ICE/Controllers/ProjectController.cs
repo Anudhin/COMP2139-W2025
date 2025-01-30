3b53183bed79849347fcb2ee462674038701cec8
Anudhin <anudhinthomas@gmail.com>
2025-01-30 16:49:51 -0500

    Clean repository reset by Anudhin

---
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -0,0 +1,53 @@
+﻿using COMP2139_ICE.Data;
+using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
+{
+    
+    private readonly ApplicationDbContext _context;
+
+    public ProjectController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var projects = _context.Projects.ToList();
+        return View(projects);
+    }
+    
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Project project)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Projects.Add(project); //Add project to database memory
+            _context.SaveChanges(); //Save changes to the database. save or persist or commit
+            return RedirectToAction("Index");
+        }
+        return View(project);
+    }
+    
+    //CRUD
+    
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        //Database
+        var project = new Project { ProjectId = id, Name = "Project 1", Description = "First Project" };
+        return View(project);
+    }
+ 
+}
---
+++ COMP2139-ICE/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_ICE.Models;
+
+public class Project
+{
+    /// <summary>
+    /// This is the primary key for the project
+    /// [Required]: Ensures that the property must be set
+    /// </summary>
+    public int ProjectId { get; set; }
+    
+    ///  <summary>
+    /// The Name of the project
+    /// [Required]: Ensures that the property must be set
+    /// </summary>
+
+    [Required]
+    
+    public required string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+    
+    public String Status { get; set; }
+}
